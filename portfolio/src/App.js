deee770fdcde545e9333dad1e43926fd15c2e575
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-30 01:09:34 +0200

    Feature: imported 3 pictures in Middle component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Top from './Components/Top/Top';
+import Middle from './Components/Middle/Middle';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Top/>
+     <Middle/>
     </div>
   );
 }
