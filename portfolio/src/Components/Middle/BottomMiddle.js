d04034ee424631d27b05be52d60fd00fb5a6e48c
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-30 02:06:54 +0200

    Feature: new content and styled component for bottommiddle

--- portfolio/src/Components/Middle/BottomMiddle.js
+++ portfolio/src/Components/Middle/BottomMiddle.js
@@ -2,17 +2,72 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledBottomMiddle = styled.div`
-
+height: 100vh;
 background-image: url('');
 clip-path: polygon(0 15%, 100% 0, 100% 85%, 0 100%);
 
+.row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+  text-align: center;
+}
+
+p {
+  margin: 0%;
+}
 `;
 
 const BottomMiddle = () => {
   return ( 
-    <div>
-
-    </div>
+    <StyledBottomMiddle>
+<div 
+className="row">
+  <div 
+  className="column">
+    {/* <img src={} alt="" /> */}
+    <h4>Front End</h4>
+    <p>HTML5</p>
+    <p>CSS3 | SASS</p>
+    <p>Javascript</p>
+    <p>REACT | REDUX</p>
+  </div>
+  <div
+  className="column">
+     {/* <img src={} alt="" /> */}
+    <h4>SERVER</h4>
+    <p>NodeJS</p>
+    <p>Express</p>
+    <p>Koa</p>
+    <p>Python</p>
+  </div>
+  <div
+  className="column">
+     {/* <img src={} alt="" /> */}
+    <h4>DATABASE</h4>
+    <p>PostgreSQL</p>
+    <p>mySQL</p>
+    <p>MongoDB</p>
+    <p>Cassandra</p>
+  </div>
+  <div
+  className="column">
+     {/* <img src={} alt="" /> */}
+    <h4>Deployment</h4>
+    <p>Systemic Design</p>
+    <p>AWS ECS | SQS</p>
+    <p>Docker</p>
+    <p>Git</p>
+  </div>
+</div>
+    </StyledBottomMiddle>
    );
 }
  
