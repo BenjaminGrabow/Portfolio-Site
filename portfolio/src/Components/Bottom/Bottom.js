defcd9bf559ab4e7d9d34b372721463b6f93f68f
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-01 12:34:30 +0200

    Feature: added card effect to all elements in Bottom

--- portfolio/src/Components/Bottom/Bottom.js
+++ portfolio/src/Components/Bottom/Bottom.js
@@ -7,13 +7,16 @@ height: 100vh;
 .row {
   display: flex;
   justify-content: space-around;
+  margin: 1.5rem;
 }
 
 .column {
   display: flex;
   flex-direction: column;
-  width: 18%;
+  width: 30rem;
+  height: 15rem;
   position: relative;
+  margin: 1rem;
 }
 
 .card {
@@ -38,12 +41,14 @@ height: 100vh;
   color: white;
 }
 
-.front {
+.back {
+  transform: rotateY(180deg);
   position: absolute;
   backface-visibility: hidden;
-  transform: rotateY(180deg);
   background: yellow;
   color: white;
+  width: 100%;
+  height: 100%;
 }
 `;
 
@@ -64,51 +69,84 @@ const Bottom = () => {
   </div>
   <div className="back" >
 
+  </div>
+  </div>
+  </div>
+  <div
+  className="column">
+    <div className="card">
+      <div className="front">
+<h3>Use My Tech Stuff</h3>
+<p>React</p>
+<p>Redux</p>
+<p>React Router</p>
+<p>API</p>
+  </div>
+  <div className="back" >
 
   </div>
   </div>
   </div>
   <div
   className="column">
-    <h3>Use My Tech Stuff</h3>
+    <div className="card">
+      <div className="front">
+<h3>Use My Tech Stuff</h3>
 <p>React</p>
 <p>Redux</p>
 <p>React Router</p>
 <p>API</p>
   </div>
-  <div
-  className="column">
-    <h3>Use My Tech Stuff</h3>
-<p>React</p>
-<p>Redux</p>
-<p>React Router</p>
-<p>API</p>
+  <div className="back" >
+
+  </div>
+  </div>
   </div>
 </div>
 <div className="row">
-  <div
-  className="column">
-<h3>Top Nine</h3>
-<p>Javascript</p>
-<p>HTML</p>
-<p>CSS</p>
-<p>LESS</p>
-  </div>
-  <div
-  className="column">
+  <div className="column">
+<div className="card">
+      <div className="front">
     <h3>Top Nine</h3>
 <p>Javascript</p>
 <p>HTML</p>
 <p>CSS</p>
 <p>LESS</p>
   </div>
+  <div className="back">
+
+  </div>
+  </div>
+  </div>
   <div
   className="column">
+    <div className="card">
+      <div className="front">
     <h3>Top Nine</h3>
 <p>Javascript</p>
 <p>HTML</p>
 <p>CSS</p>
 <p>LESS</p>
+  </div>
+  <div className="back">
+
+  </div>
+  </div>
+  </div>
+  <div
+  className="column">
+    <div className="card">
+      <div className="front">
+    <h3>Top Nine</h3>
+<p>Javascript</p>
+<p>HTML</p>
+<p>CSS</p>
+<p>LESS</p>
+  </div>
+  <div className="back">
+
+  </div>
+  </div>
   </div>
 </div>
     </StyledBottom>
