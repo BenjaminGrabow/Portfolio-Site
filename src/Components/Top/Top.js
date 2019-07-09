a0f69aa3438ef7059948f3350fd921f8d357686e
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-09 22:25:32 +0200

    Feature: added avatar to top component and deleted implemented video

--- src/Components/Top/Top.js
+++ src/Components/Top/Top.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import coding from './coding.mp4';
+import avatar from './myAvatar.png';
 import styled from 'styled-components';
 
 const StyledTop = styled.div`
 text-align: center;
-clip-path: polygon(0 0, 100% 0, 100% 81%, 0% 100%);
 height:100vh;
 width:100%;
 padding:0;
@@ -15,25 +14,6 @@ flex-direction: column;
 justify-content: center;
 align-items: center;
 font-size: 3rem;
-position: relative;
-
-.bg-video {
-position: absolute;
-top: 0;
-left: 0;
-height: 100%;
-width: 100%;
-z-index: -1;
-opacity: .85;
-overflow: hidden;
-}
-
-.bg-video__content {
-height: 100%;
-width: 100%;
--o-object-fit: cover;
-object-fit: cover;
-}
 
 h1 {
   margin: 0;
@@ -56,12 +36,7 @@ class Top extends React.Component {
   render() {
     return (
       <StyledTop>
-        <div className="bg-video">
-        <video className="bg-video__content" autoPlay muted loop>
-                        <source src={coding} type="video/mp4"/>
-                        Your browser is not supported!
-                    </video>
-        </div>
+        <img src={avatar} alt="own avatar" />
         <h1>Benjamin Grabow</h1>
         <p>Full Stack Developer</p>
         <button
