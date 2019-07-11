a336292bccdf15db351c4c51ce8b8f5fc039cb41
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-11 20:39:47 +0200

    Feature: added github icon

--- src/Components/Top/Top.js
+++ src/Components/Top/Top.js
@@ -93,7 +93,7 @@ class Top extends React.Component {
           <h2>Benjamin</h2>
           <div className="icons">
       <div id="wrapper">
-        <div className="box" id="facebook">&#62220;</div>
+        <div className="entypo-github box" id="facebook"/>
         <div className="box" id="twitter">&#62217;</div>
         <div className="box" id="google">&#62223;</div>
         <div className="box" id="linkedin">&#62232;</div>
