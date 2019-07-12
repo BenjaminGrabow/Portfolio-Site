1ae6dcd5a42f8eb96215999022f3d6f0a115939b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-12 21:39:10 +0200

    Style: added new style for Projects in Project Component

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -11,15 +11,14 @@ text-align: center;
   margin: 1.5rem;
 }
 
-
 .card {
   position: absolute;
   transform-style: preserve-3d;
   transition: all 1s ease;
   width: 100%;
   height: 100%;
-  width: 90%;
-  height: 40rem;
+  width: 70%;
+  height: 30rem;
   position: relative;
   margin: 1rem;
 
@@ -29,20 +28,19 @@ text-align: center;
 }
 
 .front {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  position: absolute;
-  backface-visibility: hidden;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898; 
- background-blend-mode: multiply,multiply;
-    color: white;
+display: flex;
+justify-content: space-between;
+position: absolute;
+backface-visibility: hidden;
+width: 100%; 
+height: 100%;
+/* background: linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898; 
+background-blend-mode: multiply,multiply;
+color: white; */
 }
 
 .left {
-width: 35%;
+width: 30%;
 display: flex;
 flex-direction: column;
 justify-content: center;
@@ -50,8 +48,14 @@ align-items: center;
 text-align: center;
 }
 
+.middle {
+ width: 60%;
+ display: flex;
+ align-items: center;
+}
+
 .right {
-width: 65%;
+width: 30%;
 display: flex;
 flex-direction: column;
 justify-content: center;
@@ -81,94 +85,50 @@ const Projects = () => {
       <div
         id="projects"
         className="row">
-          
-          <div className="card">
-            <div className="front">
-              <div className="row">
-              <div 
+        <div className="card">
+          <div className="front">
+            <div
               className="left">
               <h3>Use My Tech Stuff</h3>
-              </div>
-                <div className="right">
-              <p>React</p>
-              <p>Redux</p>
-              <p>React Router</p>
-              <p>API</p>
-              </div>
-              </div>
+            </div>
+            <div
+              className="middle">
               <img
                 src={useTech}
                 alt="Use My Tech Stuff gif" />
             </div>
-            <div className="back" >
-          </div>
-        </div>
-       
-          <div className="card">
-            <div className="front">
-              <div 
-              className="left">
-              <h3>Use My Tech Stuff</h3>
-              </div>
+            <div className="right">
               <p>React</p>
               <p>Redux</p>
               <p>React Router</p>
               <p>API</p>
             </div>
-            <div className="back" >
-          </div>
-        </div>
-
-        <div className="card">
-          <div className="front">
-            <h3>Use My Tech Stuff</h3>
-            <p>React</p>
-            <p>Redux</p>
-            <p>React Router</p>
-            <p>API</p>
           </div>
           <div className="back" >
           </div>
         </div>
       </div>
-
       <div className="row">
-       
-          <div className="card">
-            <div className="front">
-              <h3>Top Nine</h3>
-              <p>Javascript</p>
-              <p>HTML</p>
-              <p>CSS</p>
-              <p>LESS</p>
+        <div className="card">
+          <div className="front">
+            <div
+              className="left">
+              <h3>Use My Tech Stuff</h3>
             </div>
-            <div className="back">
+            <div
+              className="middle">
+              <img
+                src={useTech}
+                alt="Use My Tech Stuff gif" />
             </div>
-        </div>
-       
-          <div className="card">
-            <div className="front">
-              <h3>Top Nine</h3>
-              <p>Javascript</p>
-              <p>HTML</p>
-              <p>CSS</p>
-              <p>LESS</p>
+            <div className="right">
+              <p>React</p>
+              <p>Redux</p>
+              <p>React Router</p>
+              <p>API</p>
             </div>
-            <div className="back">
           </div>
-        </div>
-        
-          <div className="card">
-            <div className="front">
-              <h3>Top Nine</h3>
-              <p>Javascript</p>
-              <p>HTML</p>
-              <p>CSS</p>
-              <p>LESS</p>
-            </div>
-            <div className="back">
-
-            
+          <div className="back" >
           </div>
         </div>
       </div>
