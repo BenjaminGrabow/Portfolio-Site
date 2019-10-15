b57a8998ea7a1d59830a1cec278a6f407fd5a0a0
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-15 20:48:32 +0200

    added new gifs

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,23 +1,25 @@
 import React from "react";
 import Card from "./Card";
 import useTech from "./useTech.gif";
+import workout from "./workout.gif";
+import bookr from "./bookr.gif";
 import styled from "styled-components";
 
 const StyledProjects = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-
-.projects {
   display: flex;
-  flex-wrap: wrap;
-}
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-.card {
-  margin: 1rem;
-  width: 30%;
-}
+  .projects {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .card {
+    margin: 1rem;
+    width: 30%;
+  }
 `;
 
 class Projects extends React.Component {
@@ -32,53 +34,49 @@ class Projects extends React.Component {
         <h1>Projects</h1>
         <div className="projects">
           <div className="card">
-          <Card
-            header="Befit - Workout Tracker"
-            gif={useTech}
-            code_link="https://github.com/labseu2-workout-tracker"
-            project_link="www.befittracker.com"
-            main_text=""
-          />
+            <Card
+              header="Befit - Workout Tracker"
+              gif={workout}
+              code_link="https://github.com/labseu2-workout-tracker"
+              project_link="www.befittracker.com"
+              main_text=""
+            />
           </div>
           <div className="card">
-          <Card
-            
-            header="Algorithm Visualizer"
-            gif={useTech}
-            code_link="https://github.com/BenjaminGrabow/Visualize-Algorithms"
-            project_link="https://bookr-build-week.herokuapp.com/"
-            main-text=""
-          />
-          </div> 
-          <div className="card">
-          <Card
-            
-            header="Bookr - Ebay for books"
-            gif={useTech}
-            code_link="https://github.com/BenjaminGrabow/Bookr"
-            project_link="https://bookr-build-week.herokuapp.com/"
-            main-text=""
-          />
+            <Card
+              header="Bookr - Ebay for books"
+              gif={bookr}
+              code_link="https://github.com/BenjaminGrabow/Bookr"
+              project_link="https://bookr-build-week.herokuapp.com/"
+              main-text=""
+            />
           </div>
           <div className="card">
-          <Card
-            
-            header="Use My Tech Stuff"
-            gif={useTech}
-            code_link="https://github.com/BenjaminGrabow/Use-My-Tech-Stuff"
-            project_link="https://dreamy-almeida-7517a0.netlify.com/"
-            main-text=""
-          />
+            <Card
+              header="Use My Tech Stuff"
+              gif={useTech}
+              code_link="https://github.com/BenjaminGrabow/Use-My-Tech-Stuff"
+              project_link="https://dreamy-almeida-7517a0.netlify.com/"
+              main-text=""
+            />
           </div>
           <div className="card">
-          <Card
-            
-            header="Top Nine Friends"
-            gif={useTech}
-            code-link=""
-            project-link="https://hungry-kalam-7b7bce.netlify.com"
-            main-text=""
-          />
+            <Card
+              header="Algorithm Visualizer"
+              gif={useTech}
+              code_link="https://github.com/BenjaminGrabow/Visualize-Algorithms"
+              project_link="https://bookr-build-week.herokuapp.com/"
+              main-text=""
+            />
+          </div>
+          <div className="card">
+            <Card
+              header="Top Nine Friends"
+              gif={useTech}
+              code-link=""
+              project-link="https://hungry-kalam-7b7bce.netlify.com"
+              main-text=""
+            />
           </div>
         </div>
       </StyledProjects>
