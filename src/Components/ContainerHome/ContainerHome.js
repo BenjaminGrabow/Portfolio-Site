405076701a86b9fdd97d6a8b7bdd2d45f07d472c
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-19 22:10:33 +0200

    added new styling for navbar

--- src/Components/ContainerHome/ContainerHome.js
+++ src/Components/ContainerHome/ContainerHome.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Top from "../Top/Top";
-import Middle from "../Middle/Middle";
-import Projects from "../Projects/Projects";
-import Contact from "../Contact/Contact";
-import Footer from "../Footer/Footer";
+// import Top from "../Top/Top";
+// import Middle from "../Middle/Middle";
+// import Projects from "../Projects/Projects";
+// import Contact from "../Contact/Contact";
+// import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 
 const ContainerHome = () => {
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,19 +3,49 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledNavbar = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-around;
+padding-top: 2.5rem;
 
+.links {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 20%;
+}
+
+a {
+  text-decoration: none;
+  /* margin-left: 1rem; */
+}
+
+.logo {
+  text-align: center;
+  background-color: black;
+  /* color: white; */
+  width: 2rem;
+  font-size: 2rem;
+  font-weight: bold; 
+}
+
+i {
+  font-size: 1.5rem;
+}
 `;
 
 const Navbar = () => {
   return ( 
      <StyledNavbar>
-              <div>
-      <NavLink to="/contact">B</NavLink>
-      <NavLink to="/contact">PORTFOLIO</NavLink>
+              <div className="links">
+         <div className="logo">
+      <NavLink className="logo" to="/">B</NavLink>
+           </div>               
+      <NavLink to="/">PORTFOLIO</NavLink>
       <NavLink to="/contact">CONTACT</NavLink>
        </div>
        <div>
-      <i class="fa fa-moon"/>
+       <i className="fas fa-moon" />
        </div>
      </StyledNavbar>
    );
